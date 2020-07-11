0ec5587119f78fbf2a8c3771f527127aee937f3f
Eddy Jhoel <megapc18@gmail.com>
2020-07-10 22:04:50 -0500

    Add files via upload

--- Install/monitor.sh
+++ Install/monitor.sh
@@ -30,11 +30,11 @@ echo "
   <meta charset='UTF-8'>
   <meta name='viewport' content='width=device-width, initial-scale=1.0'>
   <meta http-equiv='X-UA-Compatible' content='ie=edge'>
-  <title>Monitor de Servicios By @Kalix1</title>
+  <title>Monitor de Servicios</title>
   <link rel='stylesheet' href='estilos.css'>
 </head>
 <body>
-<h1>Monitor de Servicios By @Kalix1</h1>
+<h1>Monitor de Servicios</h1>
 <p id='ultact'>Última actualización: $FECHA</p>
 <hr>
 " > $DIR/$ARCHIVO
